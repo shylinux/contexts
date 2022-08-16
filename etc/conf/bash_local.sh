a574a2c2ca655a82203b7a6c266fd3ec3ee2f992
shylinux <shylinux@163.com>
2022-08-16 10:41:37 +0800

    opt misc

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -21,13 +21,11 @@ sleep 1 && cd $CTX_ROOT && source etc/miss.sh
 return
 
 # shy
-su - shy -c "cd /home/shy/contexts/ && ./bin/ice.bin forever serve &"
+# /etc/rc.local
 cd /home/shy/contexts/usr/local/daemon/10000 && ./sbin/nginx -p $PWD
-
-# dev
-su - harveyshao -c "cd /home/harveyshao/contexts/ && ./bin/ice.bin forever serve dev dev &"
-cd /home/harveyshao/contexts/usr/local/daemon/10002 && ./sbin/nginx -p $PWD
+su - shy -c "cd /home/shy/contexts/ && ./bin/ice.bin forever start &"
+su - shy -c "cd /home/shy/contexts/usr/local/work/20220815-repos-server/ && ./bin/ice.bin forever start dev shy port 9030 nodename repos-server &"
 
 # mac
-cd /Users/harveyshao/contexts/ && ./bin/ice.bin forever serve dev shy &
+cd /Users/harveyshao/contexts/ && ./bin/ice.bin forever start dev shy &
 
