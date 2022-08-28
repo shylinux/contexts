393d4f788b5002021584d54da1e0bb80e95d5a73
shylinux <shylinux@163.com>
2022-08-28 18:15:23 +0800

    opt some

--- etc/conf/rc_local.sh
+++ etc/conf/rc_local.sh
@@ -1,7 +1,8 @@
-context=${context:=/home/shy/contexts}
+contexts=${contexts:=/home/shy/contexts}
 
-cd $context/usr/local/daemon/10000 && ./sbin/nginx -p $PWD
+cd $contexts/usr/local/daemon/10000 && ./sbin/nginx -p $PWD
+cd $contexts/usr/install/docker && dockerd --host unix://$PWD/docker.sock --pidfile $PWD/docker.pid --exec-root=$PWD/exec --data-root=$PWD/data --registry-mirror http://hub-mirror.c.163.com
 
-su - shy -c "cd $context/ && ./bin/ice.bin forever start &"
-su - shy -c "cd $context/usr/local/work/20220815-repos-server/ && ./bin/ice.bin forever start dev shy port 9030 nodename repos-server &"
+su - shy -c "cd $contexts && ./bin/ice.bin forever start &"
+su - shy -c "cd $contexts/usr/local/work/20220815-repos-server/ && ./bin/ice.bin forever start dev shy port 9030 nodename repos-server &"
 
