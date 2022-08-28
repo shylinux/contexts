ca5f1aa9215a35d3a3d441cb2d74c4d6b522b0a9
shylinux <shylinux@163.com>
2022-08-28 09:20:53 +0800

    opt some

--- src/main.go
+++ src/main.go
@@ -18,13 +18,17 @@ import (
 	_ "shylinux.com/x/golang-story/src/project"
 	_ "shylinux.com/x/golang-story/src/runtime"
 
+	_ "shylinux.com/x/golang-story/src/leveldb"
+	_ "shylinux.com/x/golang-story/src/rocksdb"
+	_ "shylinux.com/x/golang-story/src/tcmalloc"
+
+	_ "shylinux.com/x/golang-story/src/docker"
 	_ "shylinux.com/x/golang-story/src/grafana"
+	_ "shylinux.com/x/golang-story/src/kubernetes"
 	_ "shylinux.com/x/golang-story/src/prometheus"
-	_ "shylinux.com/x/linux-story/src/docker"
-	_ "shylinux.com/x/linux-story/src/ffmpeg"
-	_ "shylinux.com/x/linux-story/src/kubernetes"
 
 	_ "shylinux.com/x/linux-story/src/busybox"
+	_ "shylinux.com/x/linux-story/src/ffmpeg"
 	_ "shylinux.com/x/linux-story/src/gcc"
 	_ "shylinux.com/x/linux-story/src/gdb"
 	_ "shylinux.com/x/linux-story/src/glibc"
@@ -37,10 +41,6 @@ import (
 	_ "shylinux.com/x/nginx-story/src/server"
 	_ "shylinux.com/x/redis-story/src/client"
 	_ "shylinux.com/x/redis-story/src/server"
-	// _ "shylinux.com/x/mysql-story/src/clickhouse"
-	// _ "shylinux.com/x/mysql-story/src/elasticsearch"
-	// _ "shylinux.com/x/redis-story/src/kafka"
-	// _ "shylinux.com/x/redis-story/src/pulsar"
 )
 
 func main() { print(ice.Run()) }
