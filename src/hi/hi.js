bb471c2089df62048d68705c1312f396cdcc0c2b
harveyshao <harveyshao@tencent.com>
2022-10-27 22:28:42 +0800

    opt some

--- src/hi/hi.js
+++ src/hi/hi.js
@@ -1,8 +1,5 @@
 Volcanos(chat.ONIMPORT, {help: "导入数据", _init: function(can, msg) {
 	msg.Echo("hello world")
-	msg.Echo("hello world")
-	msg.Echo("hello world")
-	msg.Echo("hello world")
-	msg.Echo("hello world")
+	msg.Echo("<what>")
 	msg.Dump(can)
 }})
