b709d1c10bced6b35600fb730eac7e69503e3979
harveyshao <harveyshao@tencent.com>
2022-12-12 19:42:17 +0800

    opt vim

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -6,6 +6,7 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 export CTX_ROOT=${CTX_ROOT:=~/contexts}
 export LC_ALL=en_US.utf-8
 
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 ish_sys_path_load
 ish_sys_cli_prompt
 ish_sys_cli_alias vi vim
