8755fa7347b3d7eb0a0de5083b81a1b4d6e631e1
harveyshao <harveyshao@tencent.com>
2022-12-19 18:02:46 +0800

    opt some

--- src/hi/hi.js
+++ src/hi/hi.js
@@ -1,5 +1,9 @@
-Volcanos(chat.ONIMPORT, {help: "导入数据", _init: function(can, msg) {
-	msg.Echo("hello world")
-	msg.Echo("<what>")
-	msg.Dump(can)
+Volcanos(chat.ONIMPORT, {help: "导入数据", _init: function(can, msg, cb) {
+	can.page.style(can, can._output, html.HEIGHT, "200px")
+	// var ui_top = can.onappend.layout(can, can._output, "tabs-top", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
+	// var ui_bottom = can.onappend.layout(can, can._output, "tabs-bottom", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
+	// var ui_left = can.onappend.layout(can, can._output, "tabs-left", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
+	// var ui_right = can.onappend.layout(can, can._output, "tabs-right", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
+	// var ui_box = can.onappend.layout(can, can._output, "tabs-box", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
+	var ui_auto = can.onappend.layout(can, can._output, "auto")
 }})
