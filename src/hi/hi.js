7a51f891c0d14844ed8c3f2f179869551fe8e21b
harveyshao <harveyshao@tencent.com>
2023-01-12 20:21:59 +0800

    opt inner.css

--- src/hi/hi.js
+++ src/hi/hi.js
@@ -1,9 +1,18 @@
-Volcanos(chat.ONIMPORT, {help: "导入数据", _init: function(can, msg, cb) {
-	can.page.style(can, can._output, html.HEIGHT, "200px")
-	// var ui_top = can.onappend.layout(can, can._output, "tabs-top", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
-	// var ui_bottom = can.onappend.layout(can, can._output, "tabs-bottom", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
-	// var ui_left = can.onappend.layout(can, can._output, "tabs-left", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
-	// var ui_right = can.onappend.layout(can, can._output, "tabs-right", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
-	// var ui_box = can.onappend.layout(can, can._output, "tabs-box", can.core.List(5, function(item) { return {name: "hi"+item, list: [{text: "hi"+item}]} }))
-	var ui_auto = can.onappend.layout(can, can._output, "auto")
+Volcanos(chat.ONIMPORT, {help: "导入数据", _init: function(can, msg) {
+	can.page.Append(can, can._output, [{view: "img", style: {
+		"background": "url(/publish/icon/full.jpg)", "background-size": "320px", width: 16, height: 16,
+		"background-repeat": "no-repeat", "background-position-x": "-82px", "background-position-y": "-158px",
+	}}])
+	can.page.Append(can, can._output, [{view: "img", style: {
+		"background": "url(/publish/icon/full.jpg)", "background-size": "360px", width: 18, height: 18,
+		"background-repeat": "no-repeat", "background-position-x": "-93px", "background-position-y": "-177px",
+	}}])
+	can.page.Append(can, can._output, [{view: "img", style: {
+		"background": "url(/publish/icon/full.jpg)", "background-size": "480px", width: 24, height:24,
+		"background-repeat": "no-repeat", "background-position-x": "-123px", "background-position-y": "-236px",
+	}}])
+	can.page.Append(can, can._output, [{view: "img", style: {
+		"background": "url(/publish/icon/full.jpg)", "background-size": "480px", width: 480, height:1290,
+	}}])
+	can.sup.onimport.size(can.sup, can.ConfHeight()+html.ACTION_HEIGHT, can.sup.ConfWidth(480)), can.sup.onaction._output(can.sup, msg)
 }})
