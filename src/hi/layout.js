1e93dc2fbd6fdfbcfb129086909b9fa503d3efaf
harveyshao <harveyshao@tencent.com>
2023-01-17 23:28:36 +0800

    opt onappend

--- src/hi/layout.js
+++ src/hi/layout.js
@@ -1,10 +1,18 @@
 Volcanos(chat.ONIMPORT, {_init: function(can, msg) {
-	can.onappend.layout(can, can._output, "flex", [
-		{view: ["hi", html.DIV, "he"]},
-		{view: ["he", html.DIV, "he"]},
-		[
-			{view: ["hi", html.DIV, "he"]},
-			{view: ["he", html.DIV, "he"]},
+	can.onappend.layout(can, can._output, html.FLEX, [
+		{view: ["hi", html.DIV, "project"]}, [
+			[
+				{view: ["hi", html.DIV, "content"]},
+				{view: ["he", html.DIV, "profile"]},
+			],
+			{view: ["he", html.DIV, "display"]},
 		],
 	]).layout(can.ConfWidth(), can.ConfHeight())
+	can.onmotion.clear(can, can._output)
+	var list = [
+		{name: "h1", list: [{view: ["h1", html.DIV, "h1111"]}]},
+		{name: "h2", list: [{view: ["h1", html.DIV, "h222"]}]},
+		{name: "h3", list: [{view: ["h1", html.DIV, "h3333"]}]},
+	]
+	can.onappend.layout(can, can._output, "tabs-bottom", list)
 }})
