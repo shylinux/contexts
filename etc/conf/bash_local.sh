a324da2ab11eeb6b9ed78ffea5fca1bf2d10c2d3
harveyshao <harveyshao@tencent.com>
2023-01-25 00:36:00 +0800

    opt vimer.js

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -2,14 +2,12 @@
 
 echo "" > ~/.hushlogin
 export BASH_SILENCE_DEPRECATION_WARNING=1
-
 export CTX_ROOT=${CTX_ROOT:=~/contexts}
 export LC_ALL=en_US.utf-8
-
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 ish_sys_path_load
 ish_sys_cli_prompt
 ish_sys_cli_alias vi vim
 ish_sys_cli_alias t "tmux attach"
-
+[ "$PWD" = "$HOME" ] && cd ~/contexts
 [ -f ~/.bash_temp ] && source ~/.bash_temp; rm ~/.bash_temp &>/dev/null
