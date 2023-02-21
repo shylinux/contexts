aebf0e0ef5048f51ca66cf64b65915e78fd88c91
harveyshao <harveyshao@tencent.com>
2023-02-21 21:12:41 +0800

    opt some

--- src/hi/he.sh
+++ src/hi/he.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 show() {
-	echo "$TERM $PWD $HOME $PATH"
+	echo "$SHELL $TERM $PWD $HOME"
+	echo "$PATH"|tr ":" "\n"
 	echo "$ISH_CTX_SCRIPT <= $ISH_CTX_MODULE"
 	echo "hello world $content $@"
 }
