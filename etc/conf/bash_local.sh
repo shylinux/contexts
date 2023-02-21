7a2f2f2b28660101d40f257eed3ce98a2a816126
shaoying <shylinux@icloud.com>
2023-02-21 08:22:48 +0800

    opt intshell

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "" > ~/.hushlogin
-export BASH_SILENCE_DEPRECATION_WARNING=1
-export CTX_ROOT=${CTX_ROOT:=~/contexts}
 export LC_ALL=en_US.utf-8
+export BASH_SILENCE_DEPRECATION_WARNING=1
+touch ~/.hushlogin
+
+# export CTX_ROOT=${CTX_ROOT:=~/contexts}
+[ "$PWD" = "$HOME" ] && cd ~/contexts
+
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 ish_sys_path_load
+
 ish_sys_cli_prompt
 ish_sys_cli_alias vi vim
 ish_sys_cli_alias t "tmux attach"
-[ "$PWD" = "$HOME" ] && cd ~/contexts
+
 [ -f ~/.bash_temp ] && source ~/.bash_temp; rm ~/.bash_temp &>/dev/null
