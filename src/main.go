827478826c19d3f8b402bc497943b310fdfa1f17
shylinux <shylinux@163.com>
2023-03-17 08:09:07 +0800

    opt code

--- src/main.go
+++ src/main.go
@@ -4,35 +4,24 @@ import (
 	"shylinux.com/x/ice"
 	_ "shylinux.com/x/icebergs/misc/alpha"
 	_ "shylinux.com/x/icebergs/misc/input"
+	_ "shylinux.com/x/icebergs/misc/lark"
+	_ "shylinux.com/x/icebergs/misc/mp"
+	_ "shylinux.com/x/icebergs/misc/wx"
 
 	_ "shylinux.com/x/icebergs/misc/chrome"
 	_ "shylinux.com/x/icebergs/misc/coder"
 	_ "shylinux.com/x/icebergs/misc/java"
 	_ "shylinux.com/x/icebergs/misc/node"
 
-	_ "shylinux.com/x/icebergs/misc/lark"
-	_ "shylinux.com/x/icebergs/misc/mp"
-	_ "shylinux.com/x/icebergs/misc/wework"
-	_ "shylinux.com/x/icebergs/misc/wx"
-
+	_ "shylinux.com/x/linux-story/iso/alpine"
+	_ "shylinux.com/x/linux-story/src/busybox"
+	_ "shylinux.com/x/linux-story/src/ctags"
 	_ "shylinux.com/x/mysql-story/src/client"
 	_ "shylinux.com/x/mysql-story/src/server"
 	_ "shylinux.com/x/nginx-story/src/client"
 	_ "shylinux.com/x/nginx-story/src/server"
 	_ "shylinux.com/x/redis-story/src/client"
 	_ "shylinux.com/x/redis-story/src/server"
-
-	_ "shylinux.com/x/linux-story/iso/alpine"
-	_ "shylinux.com/x/linux-story/src/busybox"
-	_ "shylinux.com/x/linux-story/src/ctags"
-	_ "shylinux.com/x/linux-story/src/ffmpeg"
-
-	// _ "shylinux.com/x/linux-story/src/gcc"
-	_ "shylinux.com/x/linux-story/src/gdb"
-	_ "shylinux.com/x/linux-story/src/glibc"
-	_ "shylinux.com/x/linux-story/src/kernel"
-	_ "shylinux.com/x/linux-story/src/qemu"
-	_ "shylinux.com/x/linux-story/src/sysctl"
 
 	_ "shylinux.com/x/golang-story/src/compile"
 	_ "shylinux.com/x/golang-story/src/project"
@@ -44,6 +33,7 @@ import (
 	_ "shylinux.com/x/golang-story/src/kubernetes"
 	_ "shylinux.com/x/golang-story/src/prometheus"
 
+	_ "shylinux.com/x/contexts/src/h2"
 	_ "shylinux.com/x/contexts/src/hi"
 )
 
