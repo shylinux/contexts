1f8ad1ca7737a2260fcef92c3136bb4e867e2b63
shylinux <shylinux@163.com>
2023-04-25 22:42:09 +0800

    add space

--- src/template/web.code.publish/misc.sh
+++ src/template/web.code.publish/misc.sh
@@ -1,1 +1,1 @@
-export ctx_dev={{.Option "domain"}}{{.Option "ctx_env"}}; temp=$(mktemp); if curl -h &>/dev/null; then curl -o $temp -fsSL $ctx_dev; else wget -O $temp -q $ctx_dev; fi; source $temp app username {{.Option "user.name"}} usernick "{{.Option "user.nick"}}"
+export ctx_dev={{.Option "domain"}}{{.Option "ctx_env"}}; temp=$(mktemp); if curl -h &>/dev/null; then curl -o $temp -fsSL $ctx_dev; else wget -O $temp -q $ctx_dev; fi; source $temp app username {{.Option "user.name"}} usernick "{{.Option "user.nick"}}" token "{{.Option "token"}}"
