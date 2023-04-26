08d5fd0bd0cb2cd777c35a870555dd1aa34161ac
shylinux <shylinux@163.com>
2023-04-26 23:37:18 +0800

    opt service

--- src/main.sh
+++ src/main.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# require "shylinux.com/x/contexts/src/h2/h2.sh"
-
 main() {
 	case "$1" in
 		app) # 生产环境
 			export PATH="$PWD/bin:$PWD/local/bin:$PWD/usr/local/go/bin:$PATH"
-			shift && prepare_ice && while true; do
-				bin/ice.bin serve start dev dev "$@" && break
-			done
+			shift && prepare_ice && while true; do bin/ice.bin serve start dev dev "$@" && break; done
 			;;
 		dev) # 开发环境
 			shift && prepare_system; require miss.sh
@@ -21,7 +17,6 @@ main() {
 		   	ish_sys_dev_init >/dev/null; shift; [ -n "$*" ] && ish_sys_dev_run "$@"
 			;;
 		*)
-			# require src/hi/hi.sh
 			echo "hello world"
 			;;
 	esac
