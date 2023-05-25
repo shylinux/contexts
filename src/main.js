9bdc4efbd90e89561d293932502bbccf292da59d
shylinux <shylinux@163.com>
2023-05-25 16:00:59 +0800

    add some

--- src/main.js
+++ src/main.js
@@ -3,22 +3,15 @@ Volcanos({river: {
 		office: {name: "办公 office",  index: ["web.chat.macos.desktop", "web.wiki.feel", "web.wiki.draw", "web.wiki.data"]},
 	}},
 	project: {name: "研发群", storm: {
-		studio: {name: "研发 studio", list: [
-			{name: "vimer", help: "编辑器", index: "web.code.vimer"},
-			{name: "repos", help: "代码库", index: "web.code.git.status"},
-			{name: "favor", help: "收藏夹", index: "web.chat.favor"},
-			{name: "plan", help: "任务表", index: "web.team.plan"},
-			{name: "ctx", help: "上下文", index: "web.wiki.word"},
-		]},
+		studio: {name: "研发 studio", index: ["web.code.vimer", "web.code.git.status", "web.chat.favor", "web.team.plan", "web.wiki.word"]},
 	}},
 	profile: {name: "测试群", storm: {
-		release: {name: "发布 release", index: ["web.code.compile", "web.code.publish", "web.dream", "web.space", "web.code.git.service", "web.code.git.status"]},
-		toolkit: {name: "工具 toolkit", index: ["web.code.xterm", "web.code.vimer", "web.code.bench", "web.code.pprof"]},
+		release: {name: "发布 release", index: ["web.code.compile", "web.code.publish", "web.code.pprof", "web.code.bench", "web.dream", "web.space", "web.code.git.service", "web.code.git.status"]},
 	}},
 	operate: {name: "运维群", storm: {
 		web: {name: "应用 web", index: ["broad", "serve", "space", "dream", "cache", "spide", "share"]},
 		aaa: {name: "权限 aaa", index: ["offer", "email", "user", "totp", "sess", "role"]},
-		cli: {name: "系统 cli", index: ["cli.procstat", "qrcode", "daemon", "system", "runtime", "mirrors", "forever", "signal", "timer", "routine", "log.debug"]},
+		cli: {name: "系统 cli", index: ["qrcode", "daemon", "runtime", "cli.procstat", "cli.procinfo", "mirrors", "signal", "timer", "routine", "log.debug"]},
 		nfs: {name: "文件 nfs", index: ["dir", "cat", "tar", "pack", "tail", "trash", "server", "host", "port"]},
 	}},
 }})
