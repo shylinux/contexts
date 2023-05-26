476bda639089c3ef00a8ada96b8b34c9a6059fb4
shylinux <shylinux@163.com>
2023-05-26 15:49:55 +0800

    opt some

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Volcanos({river: {
 		release: {name: "发布 release", index: ["web.code.compile", "web.code.publish", "web.code.pprof", "web.code.bench", "web.dream", "web.space", "web.code.git.service", "web.code.git.status"]},
 	}},
 	operate: {name: "运维群", storm: {
-		web: {name: "应用 web", index: ["broad", "serve", "space", "dream", "cache", "spide", "share"]},
+		web: {name: "应用 web", index: ["broad", "serve", "space", "dream", "share"]},
 		aaa: {name: "权限 aaa", index: ["offer", "email", "user", "totp", "sess", "role"]},
 		cli: {name: "系统 cli", index: ["qrcode", "daemon", "runtime", "cli.procstat", "cli.procinfo", "mirrors", "signal", "timer", "routine", "log.debug"]},
 		nfs: {name: "文件 nfs", index: ["dir", "cat", "tar", "pack", "tail", "trash", "server", "host", "port"]},
