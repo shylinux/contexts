80f68b136aee14409de22285d3fe937788c1120b
shylinux <shylinux@163.com>
2023-05-27 12:19:37 +0800

    opt some

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ Volcanos({river: {
 	project: {name: "研发群", storm: {
 		studio: {name: "研发 studio", index: ["web.code.vimer", "web.code.git.status", "web.chat.favor", "web.team.plan", "web.wiki.word"]},
 	}},
-	profile: {name: "测试群", storm: {
+	profile: {name: "测试群", type: aaa.TECH, storm: {
 		release: {name: "发布 release", index: ["web.code.compile", "web.code.publish", "web.code.pprof", "web.code.bench", "web.dream", "web.space", "web.code.git.service", "web.code.git.status"]},
 	}},
-	operate: {name: "运维群", storm: {
+	operate: {name: "运维群", type: aaa.TECH, storm: {
 		web: {name: "应用 web", index: ["broad", "serve", "space", "dream", "share"]},
 		aaa: {name: "权限 aaa", index: ["offer", "email", "user", "totp", "sess", "role"]},
 		cli: {name: "系统 cli", index: ["qrcode", "daemon", "runtime", "cli.procstat", "cli.procinfo", "mirrors", "signal", "timer", "routine", "log.debug"]},
