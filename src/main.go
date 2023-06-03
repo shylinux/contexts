d9175929ad4efb0276fbf84e7e859730d70b4ab1
shylinux <shylinux@163.com>
2023-06-03 09:06:50 +0800

    add node

--- src/main.go
+++ src/main.go
@@ -1,5 +1,9 @@
 package main
 
-import "shylinux.com/x/ice"
+import (
+	"shylinux.com/x/ice"
+	_ "shylinux.com/x/icebergs/misc/java"
+	_ "shylinux.com/x/icebergs/misc/node"
+)
 
 func main() { print(ice.Run()) }
