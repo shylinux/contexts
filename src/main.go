240c75c7815063684a123216fc0be304425f6656
shylinux <shylinux@163.com>
2023-06-28 21:54:44 +0800

    add portal

--- src/main.go
+++ src/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"shylinux.com/x/ice"
+	_ "shylinux.com/x/icebergs/misc/alpha"
+	_ "shylinux.com/x/icebergs/misc/chrome"
 	_ "shylinux.com/x/icebergs/misc/java"
 	_ "shylinux.com/x/icebergs/misc/node"
 )
