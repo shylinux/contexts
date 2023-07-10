cf0cf30b3fc51cdacac702a282ee47fa6e056e84
shylinux <shylinux@163.com>
2023-07-10 12:48:35 +0800

    opt some

--- src/template/web.code.publish/binary.sh
+++ src/template/web.code.publish/binary.sh
@@ -1,2 +1,2 @@
 mkdir contexts; cd contexts
-ctx_dev={{.Option "domain"}} temp=$(mktemp); if curl -h &>/dev/null; then curl -o $temp -fsSL $ctx_dev; else wget -O $temp -q $ctx_dev; fi; source $temp binary
+ctx_dev={{.Option "domain"}}{{.Option "ctx_env"}} temp=$(mktemp); if curl -h &>/dev/null; then curl -o $temp -fsSL $ctx_dev; else wget -O $temp -q $ctx_dev; fi; source $temp binary
