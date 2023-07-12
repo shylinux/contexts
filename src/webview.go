0ef2a48e74806a1e47af65a33ccd2eaeca8e1eb8
shylinux <shylinux@163.com>
2023-07-12 12:50:03 +0800

    opt some

--- src/webview.go
+++ src/webview.go
@@ -6,6 +6,9 @@ import (
 	"shylinux.com/x/ice"
 	"shylinux.com/x/icebergs/base/tcp"
 	"shylinux.com/x/icebergs/base/web"
+	_ "shylinux.com/x/icebergs/misc/alpha"
+	_ "shylinux.com/x/icebergs/misc/chrome"
+	_ "shylinux.com/x/icebergs/misc/java"
 	_ "shylinux.com/x/icebergs/misc/node"
 	"shylinux.com/x/icebergs/misc/webview"
 	kit "shylinux.com/x/toolkits"
@@ -14,6 +17,6 @@ import (
 func main() {
 	os.Chdir(kit.HomePath(ice.CONTEXTS))
 	go ice.Run(web.SERVE, tcp.START)
-	defer ice.Pulse.Cmd("exit")
+	defer ice.Pulse.Cmd(ice.EXIT)
 	webview.Run(nil)
 }
