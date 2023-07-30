eeb738ba659bdda0e746e35469b138f14b0cdbb8
shylinux <shylinux@163.com>
2023-07-30 09:48:36 +0800

    add web.flows

--- src/webview.go
+++ src/webview.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"shylinux.com/x/ice"
 	_ "shylinux.com/x/icebergs/misc/alpha"
@@ -14,8 +15,10 @@ import (
 )
 
 func main() {
+	os.Setenv("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin")
 	os.Chdir(kit.HomePath(ice.CONTEXTS))
 	go ice.Run(ice.SERVE, ice.START)
 	defer ice.Pulse.Cmd(ice.EXIT)
+	time.Sleep(time.Second)
 	webview.Run(nil)
 }
