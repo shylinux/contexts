ed2601ece7e7cfeba2109122111d367a60c85d6c
shylinux <shylinux@163.com>
2023-08-03 12:17:13 +0800

    add some

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 
-export CGO_ENABLED=0
-export LC_ALL=en_US.UTF-8
-export BASH_SILENCE_DEPRECATION_WARNING=1
 touch ~/.hushlogin
+export BASH_SILENCE_DEPRECATION_WARNING=1
+export LC_ALL=en_US.UTF-8
 
-export GOPROXY=https://goproxy.cn
+export CGO_ENABLED=0
 export GOPRIVATE=shylinux.com
+export GOPROXY=https://goproxy.cn
+export GOBIN=~/contexts/usr/local/bin
+
 export CTX_ROOT=${CTX_ROOT:=~/contexts}
 [ "$PWD" = "$HOME" ] && cd ~/contexts
 [ "$PWD" = "/" ] && cd ~/contexts
 
 if uname -s|grep -v MINGW &>/dev/null; then
-	export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+	export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 fi
 ish_sys_path_load
 ish_sys_cli_prompt
 ish_sys_cli_alias vi vim
 ish_sys_cli_alias v vim
 ish_sys_cli_alias t "tmux attach"
-ish_sys_cli_alias busybox "docker run -w /root -it busybox"
-ish_sys_cli_alias alpine-dev "docker run -w /root -e 'LANG=en_US.UTF-8' -it alpine-dev sh"
-ish_sys_cli_alias alpine "docker run -w /root -e 'LANG=en_US.UTF-8' -it alpine"
-ish_sys_cli_alias centos "docker run -w /root -e 'LANG=en_US.UTF-8' -it centos"
 
-[ -f ~/.bash_temp ] && source ~/.bash_temp; rm ~/.bash_temp &>/dev/null
+# [ -f ~/.bash_temp ] && source ~/.bash_temp; rm ~/.bash_temp &>/dev/null
--- go.mod
+++ go.mod
@@ -2,11 +2,12 @@ module shylinux.com/x/contexts
 
 go 1.13
 
-// replace (
-// 	shylinux.com/x/ice => ./usr/release
-// 	shylinux.com/x/icebergs => ./usr/icebergs
-// 	shylinux.com/x/toolkits => ./usr/toolkits
-// )
+replace (
+	shylinux.com/x/go-git => ./usr/go-git
+	shylinux.com/x/ice => ./usr/release
+	shylinux.com/x/icebergs => ./usr/icebergs
+	shylinux.com/x/toolkits => ./usr/toolkits
+)
 
 require (
 	shylinux.com/x/ice v1.3.11
