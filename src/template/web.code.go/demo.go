7d415f29d0809a6dd8ce8924a2453fdc823287dd
shylinux <shylinux@163.com>
2023-08-08 03:45:21 +0800

    add some

--- src/template/web.code.go/demo.go
+++ src/template/web.code.go/demo.go
@@ -1,5 +1,15 @@
-package %s
+package {{.Option "zone"}}
 
-func init() {
-	
+import (
+	"shylinux.com/x/ice"
+)
+
+type {{.Option "name"}} struct {
+	list string `name:"list path auto" help:"示例"`
 }
+
+func (s {{.Option "name"}}) List(m *ice.Message, arg ...string) {
+	m.Echo("hello world")
+}
+
+func init() { ice.Cmd("{{.Option "key"}}", {{.Option "name"}}{}) }
