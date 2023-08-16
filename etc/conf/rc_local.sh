2653787c327e5607f2a2209f6a06a6cdfd69f60e
shy <shy@shylinux.com>
2023-08-17 00:15:30 +0800

    add some

--- etc/conf/rc_local.sh
+++ etc/conf/rc_local.sh
@@ -1,4 +1,7 @@
+
 cd /home/shy/docker && ./dockerd --host unix://$PWD/docker.sock --pidfile $PWD/docker.pid --exec-root=$PWD/exec --data-root=$PWD/data --registry-mirror "https://ccr.ccs.tencentyun.com" &
+
 contexts=${contexts:=/home/shy/contexts}
 cd $contexts/usr/local/daemon/10000 && ./sbin/nginx -p $PWD
+
 su - shy -c "cd $contexts && ./bin/ice.bin forever start &"
